44ab6e2442303dc67d7b2dadfa8532f6329e232b
alcemirjunior <alcemirmjunior@gmail.com>
2021-01-19 10:43:53 -0300

    database seeding

--- backend/src/main/java/com/github/alcemirjunior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/github/alcemirjunior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.github.alcemirjunior.dscatalog.entities.Category;
 import com.github.alcemirjunior.dscatalog.repositories.CaterogyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ public class CategoryService {
     @Autowired
     private CaterogyRepository repository;
 
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return repository.findAll();
     }
