9b808bf76ed33ea7cd091cf73b5d5133822f8b2e
alcemirjunior <alcemirmjunior@gmail.com>
2021-01-28 12:15:12 -0300

    add product listing with backend

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCard from './components/ProductCard';
 import './styles.scss';
 import { Link } from 'react-router-dom';
+import { makeRequest } from '../../core/utils/request';
+import { ProductResponse } from '../../core/types/Product';
 
- const Catalog = () => (
-    <div className="catalog-container">
+const Catalog = () => {
+  
+   //quando o componente iniciar, buscar a lista de produtos
+   //quando a lista de produtos estiver disponível, popular um estado no componente e listar os produtos dinamicamente
+
+   const [productResponse, setProductResponse] = useState<ProductResponse>();
+
+   console.log(productResponse);
+
+   useEffect(() => {
+      const params = {
+         page: 0,
+         linesPerPage: 12
+      }
+      makeRequest({url: '/products', params})
+      .then(response => setProductResponse(response.data));
+   }, []);
+  
+   return (
+      <div className="catalog-container">
          <h1 className="catalog-title">
-          Catálogo de produtos
+            Catálogo de produtos
          </h1>
          <div className="catalog-products">
-            <Link to="/products/1"><ProductCard/></Link>
-            <Link to="/products/2"><ProductCard/></Link>
-            <Link to="/products/3"><ProductCard/></Link>
-            <Link to="/products/4"><ProductCard/></Link>
-            <Link to="/products/5"><ProductCard/></Link>
-            <Link to="/products/6"><ProductCard/></Link>
-            <Link to="/products/7"><ProductCard/></Link>
-            <Link to="/products/8"><ProductCard/></Link>
-            <Link to="/products/9"><ProductCard/></Link>
-            
+            {productResponse?.content.map(product => (
+               <Link to={`/products/${product.id}`} key={product.id}>
+                  <ProductCard product={product}/>
+               </Link>
+            ))}
          </div>
-    </div>
- );
+      </div>
+   )
+};
 
- export default Catalog;
+export default Catalog;
+
+      // limitações fetch (foi implementado antes do axios):
+      // muito verboso
+      // não tem suporte nativo para ler o progresso de upload de arquivos
+      // não tem suporte nativo para enviar query strings
