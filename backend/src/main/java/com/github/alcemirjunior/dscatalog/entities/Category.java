cdef58009a6c35e05221107c3f18c0a5f5afb195
alcemirjunior <alcemirmjunior@gmail.com>
2021-02-08 16:20:04 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/github/alcemirjunior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/github/alcemirjunior/dscatalog/entities/Category.java
@@ -5,7 +5,9 @@ import org.springframework.http.ResponseEntity;
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_category")
@@ -22,6 +24,9 @@ public class Category implements Serializable {
 
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Product> products = new HashSet<>();
 
     public Category() {
     }
@@ -62,6 +67,10 @@ public class Category implements Serializable {
     @PreUpdate
     public void preUpdate(){
         updatedAt = Instant.now();
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
