d97f480a64a2395eb91e7263fdd678c577427f5c
alcemirjunior <alcemirmjunior@gmail.com>
2021-03-23 22:07:38 -0300

    create auth module

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,12 +1,52 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 import AuthCard from '../Card';
 import './styles.scss'
+import { Link } from 'react-router-dom'
+import ButtonIcon from 'core/components/buttonicon';
+
+type FormData = {
+    email: string;
+    password: string;
+}
 
 const Login = () => {
+    const { register, handleSubmit } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+        console.log(data);
+        //chamar API de autenticação
+    }
+
     return(
         <AuthCard title="login" >
-            <form className="login-form">
-                <h1>Formulario de login</h1>
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                <input type="email" 
+                className="form-control input-base margin-bottom-30" 
+                placeholder="Email"
+                name="email" 
+                ref={register}
+                />
+                <input type="password" 
+                className="form-control input-base" 
+                placeholder="Senha"
+                name="password" 
+                ref={register}
+                />
+                <Link to="/admin/auth/recover" className="login-link-recover">
+                    Esqueci a senha?
+                </Link>
+                <div className="login-submit">
+                    <ButtonIcon text="logar"/>
+                </div>
+                <div className="text-center">
+                    <span className="not-registered">
+                        Não tem Cadastro?
+                    </span>
+                    <Link to="/admin/auth/register" className="login-link-register">
+                        CADASTRAR
+                    </Link>
+                </div>
             </form>
         </AuthCard>    
     )
